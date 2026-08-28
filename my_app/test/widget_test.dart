0fe45cfeba0d1497826728ad933ad548e300209a
Igor <38285743+Bassgarik@users.noreply.github.com>
2026-08-28 20:18:33 +0300

    Initial commit: rekeens_cli MVP with create, generate, doctor, interactive mode

--- my_app/test/widget_test.dart
+++
@@ -1,9 +0,0 @@
-import 'package:flutter_test/flutter_test.dart';
-import 'package:my_app/app/app.dart';
-
-void main() {
-  testWidgets('App renders home page', (WidgetTester tester) async {
-    await tester.pumpWidget(const App());
-    expect(find.text('Welcome to my_app'), findsOneWidget);
-  });
-}
